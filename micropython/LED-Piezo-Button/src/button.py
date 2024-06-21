ac6a7aaba61f9656d10f30a671a48ae43dce4919
Jim Dodgen <jim@dodgen.us>
2024-06-21 16:29:59 -0700

    Improved comments and  reverted a call

--- micropython/LED-Piezo-Button/src/button.py
+++ micropython/LED-Piezo-Button/src/button.py
@@ -1,14 +1,18 @@
 # James Dodgen 2023, 2024
 # MIT licence
 
-# recent odd changes were because mqtt_as and async broke
-# my queue. so now just a sort of shared memory between the intwerupt routine
-# and the main process  see remote_config.py
+# this was using interupts/IRQ and now is being polled
+# the interupts messed with the async in mqtt_as.py
+# this tends to be polled at the end of the main.py loop.
+# Should come back and the the IRQ/mqtt_as problem fixed.
+# the use of polling requires this processor to run and 
+# not enter power saving sleep-and-wait mode
+# waiting for an event. 
 
 from universal_machine import machine, asyncio
 
 class button:
-	def __init__(self, pin): #, mqtt_client)
+	def __init__(self, pin, mqtt_client) # client is left here to ease fixing IRQ issue
 		self.button_pin = machine.Pin(pin, machine.Pin.IN, machine.Pin.PULL_UP)
 		 
 	def test(self):
