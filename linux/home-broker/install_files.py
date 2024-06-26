e5478877f3fe502a3e57fca4ece90da038ef2ba7
Jim Dodgen <jim@dodgen.us>
2024-06-25 22:33:06 -0700

    improvements to install
    Move stuff around and add feature to install multiples at once.

--- linux/home-broker/install_files.py
+++ linux/home-broker/install_files.py
@@ -1,20 +1,15 @@
-import pysftp
-import os
+# MIT licence Jim Dodgen 2024
+# Jim's install tool, pushes files, usualy python to remote hosts
+# see home-broker for example
+#
+# list of hosts to push one or all files to
+#
 hosts = [
 "192.168.0.193",
 "home-broker.local",
 ]
-ndx = 0
-for h in hosts:
-    print("%s) %s" % (ndx,h,))
-    ndx += 1
-print("which host? (0-%s)" % (ndx-1,))
-i = input()
-name="jim"
-word="foobar"
-host=hosts[int(i)]
-print("uploading to %s@%s" % (name, host,)) # I force access to the code for the password
-
+#
+# list of files to send
 files = [
 "src/zigbee2mqtt_manager.py",
 "src/mqtt_service_task.py",
@@ -33,13 +28,37 @@ files = [
 "src/index_html.py",
 "../../library/mqtt_hello.py",   
 ]
+#
+# code starts here.
+import pysftp
+import os
 
-try:
-    with pysftp.Connection(host, username=name, password=word) as sftp:
-        print("pushing files ...")
-        for f in files:
-            sftp.put(f)
-except: 
-    print("An error occured, typicaly cert issue,\nrunning ssh to clean it up\n")          
-    os.system("ssh %s@%s" % (name,host,))
+print(0) all")
+ndx = 1
+for h in hosts:
+    print("%s) %s" % (ndx,h,))
+    ndx += 1
+print("which?")
+i = input()
+name="jim"
+word="foobar"
+if int(i) == 0:
+    what_to_do = hosts
+else:
+    try:
+        what_to_do = [hosts[int(i-1)],] # singleton
+    except:
+        print("Not a valid entry, must be 0 to %s" % (len(hosts)+1,))
+    else:
+    for host in what_to_do:
+        print("uploading to %s@%s" % (name, host,)) # I force access to the code for the password
+        try:
+            with pysftp.Connection(host, username=name, password=word) as sftp:
+                print("pushing files ...")
+                for f in files:
+                    sftp.put(f)
+        except: 
+            print("An error occured, typicaly cert issue,\nrunning ssh to clean it up\n")          
+            os.system("ssh %s@%s" % (name,host,))
+        
 
