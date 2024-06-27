c84bf75cb135d6ae0fc7bcd74ae7772e4d080933
jdodgen <jim@dodgen.us>
2024-06-26 20:39:34 -0700

    Further improvments to install_files

--- linux/home-broker/install_files.py
+++ linux/home-broker/install_files.py
@@ -1,12 +1,23 @@
 # MIT licence Jim Dodgen 2024
-# Jim's install tool, pushes files, usualy python to remote hosts
+# Jim's install tool, pushes files, usualy python code 
+# to one or all remote hosts
 # see home-broker for example
+#
+#
+# password and name both can be put on each host
+name="jim"
+word="foobar"
 #
 # list of hosts to push one or all files to
 #
+# example:
+#["192.168.0.99","your_name","your_password"],
+# defaults to "name" and "word" if not a list, 
+# just a IP address is all that is needed
+# 
 hosts = [
-"192.168.0.193",
-"home-broker.local",
+["192.168.0.193","jim","foobar"],
+"home-broker.local"
 ]
 #
 # list of files to send
@@ -29,21 +40,17 @@ files = [
 "../../library/mqtt_hello.py",   
 ]
 #
-# password and name
-name="jim"
-word="foobar"
-#
 # code starts here.
 import pysftp
 import os
-
+print("default name[%s] password[%s]" % (name,word))
 print("0 - all hosts")
 ndx = 1
 for h in hosts:
     print("%s - %s" % (ndx,h,))
     ndx += 1
-print("which?")
-i = input()
+
+i = input("which? ")
 
 if int(i) == 0:
     what_to_do = hosts
@@ -51,15 +58,24 @@ else:
     try:
         what_to_do = [hosts[int(i)-1],] # singleton
     except:
-        print("Not a valid entry, must be 0 to %s" % (len(hosts),))
-    else:
-        for host in what_to_do:
-            print("uploading to %s@%s" % (name, host,)) # I force access to the code for the password
-            try:
-                with pysftp.Connection(host, username=name, password=word) as sftp:
-                    print("pushing files ...")
-                    for f in files:
-                        sftp.put(f)
-            except: 
-                print("An error occured, typicaly cert issue,\nrunning ssh to clean it up\n")          
-                os.system("ssh %s@%s" % (name,host,))
+        print("Not a valid index, must be 0 to %s" % (len(hosts),))
+        exit(-1)
+for h in what_to_do:
+    print("uploading to %s" % (h,)) # I force access to the code for the password
+    if isinstance(h, list):
+        host = h[0]
+        user_name = h[1]
+        password = h[2]
+    else:  # most just be the host
+        host = h
+        user_name = name # default, set above
+        password = word # default, set above
+    print("sftp  host[%s] user_name[%s] password[%s]" %(host, user_name, password,))
+    try:
+        with pysftp.Connection(host, username=name, password=word) as sftp:
+            print("pushing files ...")
+            for f in files:
+                sftp.put(f)
+    except: 
+        print("An error occured, typicaly cert issue,\nrunning ssh to clean it up\n")          
+        os.system("ssh %s@%s" % (name,host,))
