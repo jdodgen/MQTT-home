97073a7577d68ada65afeea382bc05f5177aa309
jdodgen <jim@dodgen.us>
2024-08-27 20:44:37 -0700

    Developmental save

--- linux/alertaway/src/mqtt_processor.py
+++ linux/alertaway/src/mqtt_processor.py
@@ -4,13 +4,18 @@ import time
 import threading
 import zlib
 import json
+import sqlite3
 
-def device_features_task():  # keep device features updated in the data base 
+class mqtt_task():
+    def __init__(self, queue=None, my_parent=None):
+    # keep device features updated in the data base 
     # handle the subscribe to the home-broker devices
     # for each subscribe callback compare to previous
     # update differences only NO state information
     import message
-    import queue 
+    import queue   # python queue not POSIX
+    db = sqlite3.connect(const.db_name, timeout=const.db_timeout)
+		
     q = queue.Queue() # callbacks are sent here
     msg = message.message(q) # MQTT connection tool# 
 
@@ -20,9 +25,10 @@ def device_features_task():  # keep device features updated in the data base
         (action, topic, payload) = q.get()
         if (action == "callback"):
             if (topic == const.home_MQTT_devices):
-                check_and_refresh_devices(payload)
-            else:
-                update_device_state(topic,payload)
+                check_and_refresh_devices(db,payload)
+            else: # other topics are from device subscribes
+                update_device_state(db,topic,payload)
+
 
 
     msg.publish("message_unit_test/demo_wall/set", '{"state": "on"}')
@@ -32,13 +38,13 @@ def device_features_task():  # keep device features updated in the data base
 previous_devices_dictionary = None
 
 
-def check_and_refresh_devices(payload):
+def check_and_refresh_devices(db,payload):
     global previous_devices_dictionary
     unzipped = zlib.decompress(payload)
     devices_dictionary = json.loads(unzipped)
     if previous_devices_dictionary == None:
         previous_devices_dictionary = devices_dictionary
-    compare_and_update(devices_dictionary, previous_devices_dictionary)
+    compare_and_update(db,devices_dictionary, previous_devices_dictionary)
 
     #list_all_devices(devices_dictionary)
 
@@ -58,54 +64,39 @@ def list_all_devices(dev):
     all_features = dev["features"]
     for f in all_features:
         print(f)
+
+
 FEATURES = "features"
-def compare_and_update(current, previous):
+def compare_and_update(db,current, previous):
     print(current)
     for friendly_name in (current.keys()):
         print(friendly_name)
-        if friendly_name in previous:  # it exists
+        if friendly_name in previous:  # different
             if (current[friendly_name]['description'] != previous[friendly_name]['description'] or
                current[friendly_name]['date'] != previous[friendly_name]['date'] or
                current[friendly_name]['source'] != previous[friendly_name]['source']):
-               update_device_and_features(current);
+               update_device(db, friendly_name, current);
             elif FEATURES in current[friendly_name]:  # has features check for changes
                 for feature in (current[friendly_name][FEATURES].keys()):  # each feature
                     print("\t", feature)
-                    print(current[friendly_name][FEATURES][feature])
-                    for tag in (current[friendly_name][FEATURES][feature].keys():
-                        if current[friendly_name][FEATURES][feature][tag] != previous[friendly_name][FEATURES][feature][tag]:
-                            update_device_and_features(current);
-                        
-                    
+                    current_feature =   current[friendly_name][FEATURES][feature]
+                    previous_feature = previous[friendly_name][FEATURES][feature]
+                    print(current_feature)
+                    for tag in (current_feature.keys():
+                        if current_feature[tag] != previous_feature[tag]:
+                            update_feature(db, friendly_name, current_feature);           
         else:
-            # insert it
-            pass
-        continue
-        got_match = False
-        for l in previous["devices"]:
-            if l['friendly_name'] == d['friendly_name']:
-                got_match = True    
-                if (l['description'] != d['description'] or
-                   l['date'] != d['date']):
-                    print("different", d['friendly_name'])
-                    changed_device(d)
-                else:
-                    #print("no diff",d['friendly_name'])
-                    pass
-                previous
-        if got_match == False:
-            print("found new one")
-            new_device(d)
+            insert_device(db, current)
 
-def new_device(device):
+def insert_device(device):
     print(device)
     
-def update_device_and_features(device):
+def update_device(device):
     print(device)                 
     
 
-def update_device_state(topic,state):
+def update_device_state(db,topic,state):
     pass
 
 if __name__ == "__main__":
-    device_features_task()
+    mqtt_task()
