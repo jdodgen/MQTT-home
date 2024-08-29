788aec326ba00704fbff408871f76396c3027820
jdodgen <jim@dodgen.us>
2024-08-28 21:44:08 -0700

    saved

--- linux/alertaway/src/mqtt_processor.py
+++ linux/alertaway/src/mqtt_processor.py
@@ -7,18 +7,22 @@ import json
 import sqlite3
 import message
 import queue   # python queue not POSIX
+import copy
 
 current = None
 previous = None
-
-topic_to_device_feature = None
+print("None", type(previous), type(current))
+topic_to_device_feature = {}
 
 def mqtt_task():
+    global previous
+    print("mt", type(previous), type(current))
     # keep device features updated in the data base 
     # handle the subscribe to the home-broker devices
     # for each subscribe callback compare to previous
     # update differences only NO state information
-    
+    previous = None
+    print("mt2", type(previous), type(current))
     db = sqlite3.connect(const.db_name, timeout=const.db_timeout)
     check = check_and_refresh_devices(db)	
     updates = device_state(db)
@@ -28,6 +32,7 @@ def mqtt_task():
     while True:
         (action, topic, payload) = q.get()
         if (action == "callback"):
+            print("callback",type(topic), type(payload))
             if (topic == const.home_MQTT_devices):
                 check.compare_and_update(payload)
             else: # other topics are from device subscribes
@@ -41,34 +46,36 @@ def mqtt_task():
 
 class check_and_refresh_devices():
     def __init__(self, db):
-        global current
-        global previous
         self.db = db
         self.FEATURES = "features"
 
     def compare_and_update(self, payload):
         global current
         global previous
+        print("glob", type(previous), type(current))
         unzipped = zlib.decompress(payload)
         current = json.loads(unzipped)
-        if previous == None:
-            previous = current
+        print("loaded", type(previous), type(current))
+        if previous is None:
+            previous = copy.deepcopy(current)
+            print("set previous",type(previous), type(current))
             self.check_current_against_database()
-        print(previous)
+        print("check friendly name", type(previous), type(current),type(None))
         for friendly_name in (current.keys()):
             print(friendly_name)
             if friendly_name in previous:  # exists
-                print("prev", previous[friendly_name] )
+                print(friendly_name, type(previous), type(current))
+                #print("prev", previous[friendly_name] )
                 if (current[friendly_name]['description'] != previous[friendly_name]['description'] or
                    current[friendly_name]['date']         != previous[friendly_name]['date'] or
                    current[friendly_name]['source']       != previous[friendly_name]['source']):
-                    self.update_device(friendly_name, self.current);
+                    self.update_device(friendly_name, current);
                 if self.FEATURES in current[friendly_name]:  # has features check for changes
-                    for feature in (current[friendly_name][FEATURES].keys()):  # each feature
-                        print("\t", feature)
-                        current_feature =   current[friendly_name][FEATURES][feature]
-                        previous_feature = previous[friendly_name][FEATURES][feature]
-                        print(current_feature)
+                    for feature in (current[friendly_name][self.FEATURES].keys()):  # each feature
+                        #print("\t", feature)
+                        current_feature =   copy.deepcopy(current[friendly_name][self.FEATURES][feature])
+                        previous_feature =  copy.deepcopy(previous[friendly_name][self.FEATURES][feature])
+                        # print(current_feature)
                         for tag in current_feature.keys():
                             if current_feature[tag] != previous_feature[tag]:
                                 self.update_feature(friendly_name, feature, current_feature);
@@ -88,9 +95,9 @@ class check_and_refresh_devices():
         self.now)
         cur.close()
         self.db.commit()
-        if FEATURES in current[friendly_name]:  # has features check for changes
-            for feature in (current[friendly_name][FEATURES].keys()):  # each feature
-                f = current[friendly_name][FEATURES][feature]
+        if self.FEATURES in current[friendly_name]:  # has features check for changes
+            for feature in (current[friendly_name][self.FEATURES].keys()):  # each feature
+                f = current[friendly_name][self.FEATURES][feature]
                 if f["access"] == "sub":  #device subscribes 
                     table = "publish_feature"
                 else:
@@ -109,17 +116,19 @@ class check_and_refresh_devices():
                 self.db.commit()
         
     def update_device(self,friendly_name, current):
-        print(device)                 
+        print(friendly_name)                 
         
     def update_feature(self, friendly_name, feature, current_feature):
+        global topic_to_device_feature
         # we build/rebuild the subscribed topic_to_device_feature
         # used when subscribe callbacks arrive
+        print(type(current_feature))
         targets = []
         if (current_feature["access"] == "sub"):
-            if current_feature["topic"] in self.topic_to_device_feature: 
-                targets = self.topic_to_device_feature[current_feature["topic"]]
+            if current_feature["topic"] in topic_to_device_feature: 
+                targets = topic_to_device_feature[current_feature["topic"]]
             targets.append([friendly_name, feature])
-            self.topic_to_device_feature[current_feature["topic"]] = targets
+            topic_to_device_feature[current_feature["topic"]] = targets
         print(friendly_name, current_feature)
         # now update feature
     
@@ -155,27 +164,27 @@ def list_all_devices(dev):
 
 # test area        
 if __name__ == "__main__":
+    #mqtt_task()
     db = sqlite3.connect("test.db", timeout=const.db_timeout)
     check = check_and_refresh_devices(db)	
     with open("test_json.js", 'r') as file:
         json_1 = file.read()
         #print(type(json_1))
         #print(json_1)
-    with open("test_json_with_changes.js", 'r') as file:
+    with open("test_json_with_changes.js", 'r', encoding='utf-8-sig') as file:
         json_2 = file.read()
     # sample payload 
-    payload_1= zlib.compress(bytes(json_1,'utf-8'))
-
-     # test 1  check if equal
-    previous = json_1
+    payload_1 = zlib.compress(bytes(json_1,'utf-8'))
+    print(">>>>>>%s<<<<<<" % (json_1))
+    previous = json.loads(json_1)
+    print("\n\n test 1  check if equal")
     check.compare_and_update(payload_1)  
-
+    print("\n\n test 2  check differences")
     # test 2 check when differences      
-    previous  = json_2
+    previous  = json.loads(json_2)
     check.compare_and_update(payload_1)  
-
-    # test 3 check when no previous, starting up for example
+    print("\n\n test 3  check differences")
+    # test 3 check when None previous, starting up for example
     # need to check against database, slower      
-    previous  = json_2
     check.compare_and_update(payload_1)  
                     
