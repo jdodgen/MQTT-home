bccd72b45aedf873e3d99f81664fd83754b1dbe0
Jim Dodgen <jim@dodgen.us>
2025-02-05 14:04:42 -0800

    Add files via upload

---
+++ micropython/library/alert_handler.py
@@ -0,0 +1,50 @@
+# MIT Licence copyright 2024 Jim Dodgen
+# alerts like LED and Piezo
+
+import machine
+import time
+
+class alert_handler:
+    # this is a visual and optional sound notification
+
+    def __init__(self, led_pin, piezo_pin):
+        self.led = machine.Pin(led_pin,machine.Pin.OUT)
+        self.led.off()
+        if piezo_pin:
+            self.piezo = machine.Pin(piezo_pin,machine.Pin.OUT)
+            self.piezo.off()
+        else:
+            self.piezo = None
+
+    def turn_on(self):
+        print('turning led on(1) current', self.led.value())
+        if (self.piezo and self.led.value() == 0):  # a little BEEP
+            self.beep(count=1)
+        self.led.value(1)
+
+    def turn_off(self):
+        print('turning led off(0) current', self.led.value())
+        if (self.piezo and self.led.value() == 1):  # a little BEEP
+            self.beep(count=1)
+        self.led.value(0)
+
+    def flash(self, count=0):
+        self.led.value(0)
+        while True:
+            self.led.value(1)
+            time.sleep(1)
+            self.led.value(0)
+            count -= 1
+            if (count < 1):
+                break
+            time.sleep(1)
+
+    def beep(self, count=1):
+        while True:
+            self.piezo.on()
+            time.sleep(0.2)
+            self.piezo.off()
+            count -= 1
+            if (count < 1):
+                break
+            time.sleep(0.2)
