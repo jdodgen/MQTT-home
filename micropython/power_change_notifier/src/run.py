f79cb1fcf2f04b0076335104c79b5e663d082496
Jim Dodgen <jim@dodgen.us>
2025-04-29 10:41:58 -0700

    added Cc: to smtp.write of Subject

--- micropython/power_change_notifier/src/run.py
+++ micropython/power_change_notifier/src/run.py
@@ -62,7 +62,7 @@ async def send_email(subject, body, cluster_id_only=False):
             smtp.login(cfg.gmail_user, cfg.gmail_password)
             smtp.to(cfg.send_messages_to, mail_from=cfg.gmail_user)
             id = cfg.cluster_id if cluster_id_only else cfg.cluster_id+":"+cfg.publish
-            smtp.write("Subject:[PCN %s] %s\n\n%s\n" % (id, subject, body,))
+            smtp.write("Cc: %s\nSubject:[PCN %s] %s\n\n%s\n" % (cfg.cc_string, id, subject, body,))
             smtp.send()
             smtp.quit()
         except:
