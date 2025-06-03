e53045dfedde90551993713b62c8c24a95c655d1
Jim Dodgen <jim@dodgen.us>
2025-06-03 13:07:58 -0700

    Update test.py

--- micropython/power_change_notifier/src/test.py
+++ micropython/power_change_notifier/src/test.py
@@ -2,13 +2,8 @@ import network
 import utime
 from umqtt.simple import MQTTClient
 
-
-# esptool.py --chip esp32s2 --port /dev/ttyACM0 write_flash -z 0x1000 ESP32_GENERIC_S2-20250415-v1.25.0.bin
-# ampy --port /dev/ttyACM0 put test_ssl_mqtt.py
-# import test_ssl_mqtt.py
-
-def cback(t,m):
-    print(t,p)
+def cback(t,m): # call back
+    print("call back:", t, p)
     
 def connectMQTT():
     client = MQTTClient(client_id=b"jims_test",
@@ -22,6 +17,7 @@ def connectMQTT():
     client.connect()
     client.set_callback(cback)
     return client
+    
 ### start ###
 ssid = "danger"
 password = "will roberson"
@@ -36,11 +32,11 @@ try:
         print("ERROR_BAD_PASSWORD")               
     elif (wifi.status() == network.STAT_NO_AP_FOUND):
         print("ERROR_AP_NOT_FOUND")
-
     print("wifi.connect wifi.status", wifi.status())
 except:
     print("exception doing wifi.connect status[]", wifi.status(),"]")
 
+### now test mqtt/ssl
 topic="test_topic"
 value="test_value"  
  
