856900da5d8bf66ab57b7f7651e1860e0e52fad7
Jim Dodgen <jim@dodgen.us>
2025-06-02 23:47:17 -0700

    added ussl

--- micropython/library/mqtt_as_lite.py
+++ micropython/library/mqtt_as_lite.py
@@ -9,8 +9,7 @@
 # Tested on ESP32-S2
 
 import gc
-#import usocket as socket
-import socket
+import usocket as socket
 import ustruct as struct
 
 gc.collect()
@@ -337,7 +336,7 @@ class MQTT_base:
         await asyncio.sleep_ms(_DEFAULT_MS)
         print("_broker_connect socket created")
         if self._ssl:
-            import ssl
+            import ussl as ssl
             self._sock = ssl.wrap_socket(self._sock, **self._ssl_params)
         premsg = bytearray(b"\x10\0\0\0\0\0")
         msg = bytearray(b"\x04MQTT\x04\0\0\0")  # Protocol 3.1.1
