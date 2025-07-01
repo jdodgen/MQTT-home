a19bb6f2abbfd4d1ea478fbaab96915a6f6da0cd
jdodgen <jim@dodgen.us>
2025-06-30 23:00:55 -0700

    removing commented dead code and other cleanup

--- micropython/power_change_notifier/src/run.py
+++ micropython/power_change_notifier/src/run.py
@@ -19,14 +19,15 @@ import time
 our_status    = feature_power.feature(cfg.cluster_id+"/"+cfg.publish, publish=True)   # publisher
 print(our_status.topic())
 
-other_status = []
-#list_of_others = []
-list_of_others = [our_status.topic(),] # use this to get echo msgs back during testing
+#other_device_features = []
+#other_device_topics = []
+other_device_topics = [our_status.topic(),] # use this to get echo msgs back during testing
+
 for dev in cfg.devices_we_subscribe_to:
     print("subscribing to:", dev)
-    other_status.append(feature_power.feature(cfg.cluster_id+"/"+dev, subscribe=True))
-    list_of_others.append(feature_power.feature(cfg.cluster_id+"/"+dev, subscribe=True).topic())
-print("list of others", list_of_others)
+    #other_device_features.append(feature_power.feature(cfg.cluster_id+"/"+dev, subscribe=True))
+    other_device_topics.append(feature_power.feature(cfg.cluster_id+"/"+dev, subscribe=True).topic())
+print("list of others", other_device_topics)
 
 wildcard_subscribe = feature_power.feature(cfg.cluster_id+"/+", subscribe=True)
 print(wildcard_subscribe.topic())
@@ -101,7 +102,7 @@ async def send_email(subject, body, cluster_id_only=False):
 
 async def raw_messages(queue):  # Process all incoming messages
     global led
-    global other_status
+    global other_device_topics
     print(".raw_messages")
     async for btopic, bmsg in queue:
         topic = btopic.decode('utf-8')
@@ -109,22 +110,24 @@ async def raw_messages(queue):  # Process all incoming messages
         print(".raw_messages [%s][%s]" % (topic, msg))
         #print("callback start_time(s)", cfg.start_time)
         i=0
-        restored_sensors = ""
-        for dev in other_status:
-            if (topic == dev.topic()):  # just getting the published message means utility outlet is powered, payload not important
+        information_text = "" # only one email for many outages
+        for device_topic in other_device_topics:
+            if (topic == device_topic):  # just getting the published message means utility outlet is powered, payload not important
+                print(".raw_messages matched one we watch")
                 cfg.got_other_message[i] = True
                 if cfg.have_we_sent_power_is_down_email[i]:
+                    print(".raw_messages  have_we_sent_power_is_down_email true")
                     cfg.have_we_sent_power_is_down_email[i] = False
                     cfg.publish_cycles_without_a_message[i] = 0
                     seconds = time.time() - cfg.start_time[i]
                     hours = seconds/3600
                     minutes = seconds/60
-                    restored_sensors +=  ("# Power restored to [%s]\n# Down, Minutes: %.f (Hours: %.1f)\n" %
-                        (cfg.devices_we_subscribe_to[i], minutes, hours))
+                    information_text +=  ("# Power restored to [%s]\n# Down, Minutes: %.f (Hours: %.1f)\n" %
+                                                    (cfg.devices_we_subscribe_to[i], minutes, hours))
                     cfg.start_time[i]=0
             i += 1
-        if restored_sensors:
-            await send_email("Power restored", restored_sensors+make_email_body())
+        if information_text:
+            await send_email("Power restored", information_text+make_email_body())
 
 # DEBUG: show RAM messages.
     async def _memory(self):
@@ -164,19 +167,7 @@ async def problem_reporter(error_queue):
                 else:
                     break
 
-'''async def problem_reporter(error_code, repeat=1):
-    if error_code >  0:
-        led.turn_off()
-        await asyncio.sleep(2)
-        x = 0
-        while x < repeat:
-            await led.async_flash(count=error_code, duration=0.4, ontime=0.4)
-            x += 1
-            await asyncio.sleep(1)
-        await asyncio.sleep(2)'''
-
 async def main():
-    global other_status
     global our_status
     global led
 
@@ -191,78 +182,34 @@ async def main():
             broker=cfg.broker,
             broker_user=cfg.user,
             broker_password=cfg.password,
-            subscriptions =  list_of_others,  # list_of_topics,
-            # publish_retains = retains,     #
+            subscriptions =  other_device_topics,  # list_of_topics,
+            # publish_retains = retains,     # need to add hello introduction
             )
     asyncio.create_task(raw_messages(mqtt.queue)) # incoming messages
-    asyncio.create_task(problem_reporter(mqtt.error_queue)) # errors
-    #asyncio.create_task(mqtt.monitor_wifi())  # connects and reconnects as needed
-    #asyncio.create_task(mqtt.process_incoming_messages())
-    #await asyncio.sleep(1)
+    asyncio.create_task(problem_reporter(mqtt.error_queue)) # error number
+
     print(".main connecting to broker")
     client = await mqtt.broker()  # first connection to the broker.
     print(".main client connected")
     await client.publish(our_status.topic(), "client connected")
-    '''
-    print("creating tasks")
-    # these are needed for mqtt_as_lite
-    # they run forever and fix connection problems
-    asyncio.create_task(client.monitor_wifi())
-    asyncio.create_task(client._handle_msg())
-    asyncio.create_task(client.monitor_broker())
-
-    # this pulls messages from the queue
-    #asyncio.create_task(raw_messages(client))
-    #
-    #done above'''
 
     await mqtt.wifi_up.wait()
     print(".main emailing startup")
     # await send_email("Starting" % (cfg.cluster_id, cfg.publish),errors_msg)
     await send_email("Starting", boilerplate)
-    #
-    # now checking on the broker connect. It too long then email
-    '''
-    start_broker_connect = time.time()
-    too_long = 60
-    broker_not_up_send_email = False
-    while True:
-        elapse  = time.time() - start_broker_connect
-        huh = client.broker_connected.is_set()
-        #print("elapse", elapse, huh)
-        if huh:
-            break
-        if elapse > too_long and not broker_not_up_send_email:
-            broker_not_up_send_email = True
-            print("email no broker")
-            await send_email("broker not found", "broker at ["+cfg.broker+"] not found")
-        await asyncio.sleep(1)
-
-    await client.broker_connected.wait()
-    if broker_not_up_send_email:
-        await send_email("Connected", "Broker now connected")
-    '''
 
     #resub_loop_count = 0
     while True:  # top loop checking to see of other has published
-        #await asyncio.sleep(120)
-        # await client.publish(our_status.topic(), our_status.payload_on())
-        # await asyncio.sleep(120)
-        # await asyncio.sleep(2)
-        # await client.publish(our_status.topic(), our_status.payload_on())
-        # await asyncio.sleep(2)
         await client.publish(our_status.topic(), our_status.payload_on())
-        #await client.subscribe(wildcard_subscribe.topic())
-        # continue
         i=0
-        down_sensors = 0
+        down_device_cnt = 0
         print(".main \b[publish_check_loop]")
         for stat in  cfg.got_other_message:
             if stat == False:  # no message(s) this cycle
                 if (cfg.publish_cycles_without_a_message[i] > cfg.other_message_threshold):
                     if (cfg.start_time[i] == 0):
                         if (not cfg.have_we_sent_power_is_down_email[i]):
-                            down_sensors += 1
+                            down_device_cnt += 1
                             cfg.have_we_sent_power_is_down_email[i]= True
                             cfg.start_time[i] = time.time()
                 else:
@@ -277,16 +224,8 @@ async def main():
         else:
             print(".main turning led off", cfg.start_time)
             led.turn_off()
-        if down_sensors:
+        if down_device_cnt:
             await send_email("Power Outage(s)", make_email_body(), cluster_id_only=True)
-        # if client.do_subscribes or cfg.subscribe_interval < resub_loop_count:
-            # client.do_subscribes = False
-            # resub_loop_count = 0
-            # print("subscribing ", wildcard_subscribe.topic())
-            # await client.subscribe(wildcard_subscribe.topic())
-            # #for dev in other_status:
-                # #await client.subscribe(dev.topic())
-        # resub_loop_count += 1
         await asyncio.sleep(cfg.number_of_seconds_to_wait)
 
 def make_email_body():
