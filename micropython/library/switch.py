e4f97bcb73c4293ea3755e80f8aa85e495a4b450
jdodgen <jim@dodgen.us>
2025-07-27 11:10:22 -0700

    added switch.py to micropython library

---
+++ micropython/library/switch.py
@@ -0,0 +1,19 @@
+# copyrite James Dodgen 2025
+# MIT license
+
+# typically switch.test() is called in the main.py asyncio
+# loop hopefully with something to do or a sleep(0.3).
+#
+
+from universal_machine import machine
+class switch:
+    def __init__(self, pin, mqtt_client):
+        self.mqtt_client = mqtt_client # client is here when IRQ fixed
+        self.button_pin = machine.Pin(pin, machine.Pin.IN, machine.Pin.PULL_UP)
+
+    def test(self):
+        value = self.button_pin.value()
+        # print("switch value[%s]" % (value,))
+        return value
+
+# switch(1,2)
