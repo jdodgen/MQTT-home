8e88c95a7bf3fa46d713544f83387f52cec578e8
jdodgen <jim@dodgen.us>
2025-08-04 17:37:42 -0700

    Finished descripting

--- micropython/power_change_notifier/src/install.py
+++ micropython/power_change_notifier/src/install.py
@@ -57,7 +57,7 @@ import datetime
 import tomllib
 import sys
 
-# change these as needed
+###### modify these as needed ######
 mp_lib_offset="../../library/"  # micropython specific
 all_lib_offset="../../../library/" # both linux and micropython
 cluster_lib = str(Path.home())+"/Dropbox/wip/pcn_clusters"
@@ -75,25 +75,25 @@ print("Device on:", serial_port)
 current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(current_dir, all_lib_offset))
 import feature_power # located in all_lib_offset
-# end of stuff that needs modification
+###### end of stuff that needs modification ######
 
 def load_cluster(cluster_file_name):
-	if len(cluster_file_name) > 1:
-		cluster_toml = cluster_lib+"/"+cluster_file_name
-	else:
-		print("testing from current directory")
-		cluster_toml = "cluster-example.toml"  # test cluster
-	try:
-		with open(cluster_toml, 'rb') as toml_file:
-			cluster = tomllib.load(toml_file)
-			return cluster
-				# print(cluster)
-	except FileNotFoundError:
-		print("Error: ",cluster_toml," File not found")
-		sys.exit()
-	except tomllib.TOMLDecodeError as e:
-		print("Error: Invalid TOML format in {file_path}: {e}")
-		sys.exit()
+    if len(cluster_file_name) > 1:
+        cluster_toml = cluster_lib+"/"+cluster_file_name
+    else:
+        print("testing from current directory")
+        cluster_toml = "cluster-example.toml"  # test cluster
+    try:
+        with open(cluster_toml, 'rb') as toml_file:
+            cluster = tomllib.load(toml_file)
+            return cluster
+                # print(cluster)
+    except FileNotFoundError:
+        print("Error: ",cluster_toml," File not found")
+        sys.exit()
+    except tomllib.TOMLDecodeError as e:
+        print("Error: Invalid TOML format in {file_path}: {e}")
+        sys.exit()
 
 def print_sensors(sensors):
     sensor_keys = list(sensors.keys())
@@ -112,82 +112,77 @@ def print_sensors(sensors):
             sys.exit()
 
 class create_cfg:
-	def __init__(cluster, sensor_to_make):
-		self.cluster = cluster
-		self.sensor_to_make = sensor_to_make
-		self.sensors = self.cluster["sensor"]
-		self.set_cfg_values()
-		self.write_cfg()
+    def __init__(self, cluster, sensor_to_make):
+        self.cluster = cluster
+        self.sensor_to_make = sensor_to_make
+        self.sensors = self.cluster["sensor"]
+        self.our_feature = feature_power.feature(self.make_topic(sensor_to_make), publish=True)   # publisher
+        print(self.our_feature.topic())
+        self.set_cfg_values()
+        self.create_hard_tracked_topics()
+        self.write_cfg()
 
-	def set_cfg_values(self,):
-		if sensor_to_make in sensor_to_makesensors:
-			desc = self.sensors[self.sensor_to_make].get("desc")
-			self.publish_to = self.sensor_to_make+" "+desc if desc else self.sensor_to_make
-			self.send_email = self.sensors[self.sensor_to_make].get("send_email",False)
-			self.ssid = sensors[sensor_to_make].get("ssid", cluster["network"]["ssid"])
-			self.wifi_password = self.sensors[sensor_to_make].get("wifi_password", cluster["network"]["wifi_password"])
-			self.monitor_only = sensors[sensor_to_make].get("monitor_only", False)
-			self.switch = sensors[sensor_to_make].get("switch", False)
-			self.switch_type = sensors[sensor_to_make].get("switch_type","NO")
-		else:  # these "letters" do not exist in the toml file but are treated as "soft_tracking"  that is not tracked until first publish
-			self.publish_to = sensor_to_make   # single letter version
-			self.send_email = False
-			self.ssid = cluster["network"]["ssid"]
-			self.wifi_password = cluster["network"]["wifi_password"]
-			self.monitor_only = False
-			self.switch = False
-			self.switch_type = False
-		print("publish_to [%s] send_email [%s] ssid[%s] pw[%s] monitor_only [%s] switch [%s] switch_type [%s]" %
-			(self.publish_to, self.send_email, self.ssid, self.wifi_password, self.monitor_only, self.switch, self.switch_type))
-		self.email_addresses()
+    def set_cfg_values(self,):
+        if self.sensor_to_make in self.sensors:
+            desc = self.sensors[self.sensor_to_make].get("desc")
 
-	def make_topic(self):
-		#if sensor_to_make in self.sensors:
-		self.desc = self.sensors.get(sensor_to_make, "")
-			# if "desc" in self.sensors[sensor_to_make]:
-				# desc = self.sensors[sensor_to_make]["desc"]
-			# else:
-				# desc = ''
-			# else:
-				# desc = ''
-		if self.desc == '':
-			return cluster["cluster_id"]+"/"+self.sensor_to_make
-		else:
-			return cluster["cluster_id"]+"/"+self.sensor_to_make+" "+self.desc
-		self.our_feature = feature_power.feature(make_topic_cluster_pub(publish_to), publish=True)   # publisher
-		print(self.our_feature.topic())
+            #self.publish_to = self.sensor_to_make+" "+desc if desc else self.sensor_to_make
 
-		# build features
-		#our_feature    = feature_power.feature(make_topic_cluster_pub(publish_to), publish=True)   # publisher
-		# print(our_feature.topic())
-		
-	def email_addresses(self):
-		self.cc_string = ''
-		for addr in self.cluster["email"]["to_list"]:
-			self.cc_string += "<%s>," % (addr,)
-		self.cc_string = self.cc_string.rstrip(",")
-		print(self.cc_string)
-		return self.cc_string
+            self.send_email = self.sensors[self.sensor_to_make].get("send_email",False)
+            self.ssid = self.sensors[self.sensor_to_make].get("ssid", self.cluster["network"]["ssid"])
+            self.wifi_password = self.sensors[self.sensor_to_make].get("wifi_password", self.cluster["network"]["wifi_password"])
+            self.monitor_only = self.sensors[self.sensor_to_make].get("monitor_only", False)
+            self.switch = self.sensors[self.sensor_to_make].get("switch", False)
+            self.switch_type = self.sensors[self.sensor_to_make].get("switch_type","NO")
+        else:  # these "letters" do not exist in the toml file but are treated as "soft_tracking"  that is not tracked until first publish
+            #self.publish_to = self.sensor_to_make   # single letter version
+            self.send_email = False
+            self.ssid = self.cluster["network"]["ssid"]
+            self.wifi_password = self.cluster["network"]["wifi_password"]
+            self.monitor_only = False
+            self.switch = False
+            self.switch_type = False
+        print("send_email [%s] ssid[%s] pw[%s] monitor_only [%s] switch [%s] switch_type [%s]" %
+            (self.send_email, self.ssid, self.wifi_password, self.monitor_only, self.switch, self.switch_type))
+        self.email_addresses()
 
-	# cc_string = email_addresses(cluster)
+    def make_topic(self, key):
+        #if sensor_to_make in self.sensors:
+        print("make_topic", key)
+        print("cluster_id",self.cluster["cluster_id"])
+        sensor = self.sensors.get(key)
+        if sensor:
+            desc = sensor.get("desc", "")
+        else:
+            desc = ""
+        if desc == '':
+            name =  self.cluster["cluster_id"]+"/"+key
+        else:
+            name =  self.cluster["cluster_id"]+"/"+key+" "+desc
+        return name
 
-	def create_hard_tracked_topics(self):
-		sensor_keys = list(self.sensors.keys())
-		self.hard_tracked_topics = [] # these get tracked from boot. others (soft) only after first publish
-		for key in sensor_keys:
-			if self.sensors[key].get("soft_tracking") == True or self.sensors[key].get("monitor_only") == True:
-					continue
-			self.hard_tracked_topics.append(feature_power.feature(self.make_topic(key), subscribe=True).topic())
+    def email_addresses(self):
+        self.cc_string = ''
+        for addr in self.cluster["email"]["to_list"]:
+            self.cc_string += "<%s>," % (addr,)
+        self.cc_string = self.cc_string.rstrip(",")
+        print(self.cc_string)
+        return self.cc_string
 
-		#hard_tracked_topics = create_hard_tracked_topics(sensors, sensor_keys)
+    # cc_string = email_addresses(cluster)
 
-		print("hard tracked topics", hard_tracked_topics)
-
-#print("\nflashing ssid[%s] device[%s]\n" % (ssid, publish_to,))
+    def create_hard_tracked_topics(self):
+        sensor_keys = list(self.sensors.keys())
+        self.hard_tracked_topics = [] # these get tracked from boot. others (soft) only after first publish
+        for key in sensor_keys:
+            if self.sensors[key].get("soft_tracking") == True or self.sensors[key].get("monitor_only") == True:
+                    continue
+            self.hard_tracked_topics.append(feature_power.feature(self.make_topic(key), subscribe=True).topic())
+        print("hard tracked topics", self.hard_tracked_topics)
 
 # this is the cfg.py template uses % to pass in stuff
-	def write_cfg(self):
-		cfg_template = """
+    def write_cfg(self):
+        cfg_template = """
 # MIT license copyright 2024, 2025 Jim Dodgen
 # this cfg.py was created by: install.py
 # Date: %s
@@ -228,57 +223,32 @@ monitor_only = %s  # if True this sensor does not publish status and therefore i
 switch = %s # if true then "switch_gpio" is tested if off then no publish will be sent
 switch_type = "%s" # for "NO or NC defaults to "NO". So when "closed" no "power" publishes are sent
 """
-		now = datetime.datetime.now()
-		cfg_text =  cfg_template % (now.strftime("%Y-%m-%d %H:%M:%S"),
-			self.ssid,
-			self.wifi_password,
-			self.cluster["mqtt_broker"]["broker"],
-			self.cluster["mqtt_broker"]["ssl"],
-			self.cluster["mqtt_broker"]["user"],
-			self.cluster["mqtt_broker"]["password"],
-			self.cluster["email"]["to_list"],
-			self.cluster["email"]["gmail_password"],
-			self.cluster["email"]["gmail_user"],
-			self.cc_string,
-			self.publish_to,
-			self.cluster["cluster_id"],
-			self.send_email,
-			self.hard_tracked_topics,
-			self.monitor_only,
-			self.switch,
-			self.switch_type)
-		#print("[%s][%s] [%s]\n%s [%s][%s]\n" % (ssid, wifi_password, broker, to_list,
-		#   gmail_password, gmail_user ))
-		with open('cfg.py', 'w') as f:
-			f.write(cfg_text)
-		print("created cfg.py")
+        now = datetime.datetime.now()
+        cfg_text =  cfg_template % (now.strftime("%Y-%m-%d %H:%M:%S"),
+            self.ssid,
+            self.wifi_password,
+            self.cluster["mqtt_broker"]["broker"],
+            self.cluster["mqtt_broker"]["ssl"],
+            self.cluster["mqtt_broker"]["user"],
+            self.cluster["mqtt_broker"]["password"],
+            self.cluster["email"]["to_list"],
+            self.cluster["email"]["gmail_password"],
+            self.cluster["email"]["gmail_user"],
+            self.cc_string,
+            self.our_feature.topic(),
+            self.cluster["cluster_id"],
+            self.send_email,
+            self.hard_tracked_topics,
+            self.monitor_only,
+            self.switch,
+            self.switch_type)
+        #print("[%s][%s] [%s]\n%s [%s][%s]\n" % (ssid, wifi_password, broker, to_list,
+        #   gmail_password, gmail_user ))
+        with open('cfg.py', 'w') as f:
+            f.write(cfg_text)
+        print("created cfg.py")
 
-cluster = load_cluster(sys.argv[1])
-print_sensors(cluster["sensor"])
-print("select one (case insensitive): ", end="")
-sensor_to_make = input().upper()
-print("request = ", sensor_to_make)
-create_cfg(cluster, sensor_to_make) # drops cfg.py file
-
-# install micropython kernal
-did_we_flash = False
-print ("press and hold O (flat side)\nthen press RST (indent) momentary\nrelease O\nto allow flashing micropython")
-print("install micropython? (y,N)")
-ans = input()
-if (ans.upper() == "Y"):
-    did_we_flash = True
-    os.system("esptool.py --port /dev/ttyACM0 erase_flash")
-    # os.system("esptool.py --chip esp32s2 --port /dev/ttyACM0 write_flash -z 0x1000 ESP32_GENERIC_S2-20241129-v1.24.1.bin")
-    os.system("esptool.py --chip esp32s2 --port /dev/ttyACM0 write_flash -z 0x1000 ESP32_GENERIC_S2-20250415-v1.25.0.bin")
-    print("\npress RST on esp32-s2 to reset (in the indent)")
-    input()
-# install library code
-if did_we_flash == False:
-    print("install library code? (y,N)")
-    lans = input()
-else:
-    lans = "Y"
-if (lans.upper() == "Y"):
+def push_library_code():
     code = [
     mp_lib_offset+"main.py",
     mp_lib_offset+"boot.py",
@@ -296,13 +266,7 @@ if (lans.upper() == "Y"):
         print("installing", c)
         os.system("ampy --port %s put %s" % (serial_port,c))
 
-# install application code
-if did_we_flash == True or lans == "Y":
-	 ans = "Y"
-else:
-    print("\ninstall application code? (Y,n)")
-    ans = input()
-if (ans.upper() != "N"):
+def push_application_code():
     code = [
     "run.py",
     "cfg.py",
@@ -311,11 +275,48 @@ if (ans.upper() != "N"):
     for c in code:
         print("installing", c)
         os.system("ampy --port %s put %s" % (serial_port,c))
-        
-os.system("ampy --port %s ls" % (serial_port,))
-if os.name == 'nt':
-    print("\n  putty -serial ", serial_port)
-else:
-    print("\n  picocom -b 115200 ", serial_port)
 
+def main():
+    cluster = load_cluster(sys.argv[1])
+    print_sensors(cluster["sensor"])
+    print("select one (case insensitive): ", end="")
+    sensor_to_make = input().upper()
+    print("request = ", sensor_to_make)
+    create_cfg(cluster, sensor_to_make) # drops cfg.py file
 
+    # install micropython kernal
+    did_we_flash = False
+    print ("press and hold O (flat side)\nthen press RST (indent) momentary\nrelease O\nto allow flashing micropython")
+    print("install micropython? (y,N)")
+    ans = input()
+    if (ans.upper() == "Y"):
+        did_we_flash = True
+        os.system("esptool.py --port /dev/ttyACM0 erase_flash")
+        # os.system("esptool.py --chip esp32s2 --port /dev/ttyACM0 write_flash -z 0x1000 ESP32_GENERIC_S2-20241129-v1.24.1.bin")
+        os.system("esptool.py --chip esp32s2 --port /dev/ttyACM0 write_flash -z 0x1000 ESP32_GENERIC_S2-20250415-v1.25.0.bin")
+        print("\npress RST on esp32-s2 to reset (in the indent)")
+        input()
+    # install library code
+    if did_we_flash == False:
+        print("install library code? (y,N)")
+        lans = input()
+    else:
+        lans = "Y"
+    if (lans.upper() == "Y"):
+        push_library_code()
+    # install application code
+    if did_we_flash == True or lans.upper() == "Y":
+         ans = "Y"
+    else:
+        print("\ninstall application code? (Y,n)")
+        ans = input()
+    if (ans.upper() != "N"):
+        push_application_code()
+    os.system("ampy --port %s ls" % (serial_port,))
+    if os.name == 'nt':
+        print("\n  putty -serial ", serial_port)
+    else:
+        print("\n  picocom -b 115200 ", serial_port)
+
+if __name__ == "__main__":
+    main()
