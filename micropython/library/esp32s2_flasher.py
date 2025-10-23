ac0f56c51f1a47c8f475209c2d0ae2c714350368
Jim Dodgen <jim@dodgen.us>
2025-10-23 14:33:03 -0900

    Update esp32s2_flasher.py

--- micropython/library/esp32s2_flasher.py
+++ micropython/library/esp32s2_flasher.py
@@ -2,13 +2,27 @@
 import os 
 import time
 
-micropython_bin = "/home/jim/Downloads/ESP32_GENERIC_S2-20250911-v1.26.1.bin"
+micropython_bin = "ESP32_GENERIC_S2-20250911-v1.26.1.bin"
 # micropython_bin = "/home/jim/Downloads/ESP32_GENERIC_S2-20250415-v1.25.0.bin"
 
-def flasher():
-    os.system("esptool --after no-reset --chip esp32s2 --port /dev/ttyACM0 erase-flash")
-    cmd = "esptool --after hard-reset --chip esp32s2 --port /dev/ttyACM0 write-flash -z 0x1000 %s" % (micropython_bin,)
-    print(cmd)
-    time.sleep(3)
-    os.system(cmd)
-    time.sleep(5)
+class flasher:
+    def __init__(self): 
+        global micropython_bin
+        if os.name == 'nt':
+            self.serial_port = "COM3"
+            self.micropython_path = "/Users/jim/Downloads/"+micropython_bin
+        else: # linux
+            serial_port = "/dev/ttyACM0"
+            self.micropython_path = "/home/jim/Downloads/"+micropython_bin
+        print("Device on:", self.serial_port)
+         
+    def port(self):
+        return self.serial_port
+
+    def flash(self):
+        os.system("esptool --after no-reset --chip esp32s2 --port %s erase-flash" % (self.serial_port,))
+        cmd = "esptool --after hard-reset --chip esp32s2 --port %s write-flash -z 0x1000 %s" % (self.serial_port, self.micropython_bin,)
+        print(cmd)
+        time.sleep(3)
+        os.system(cmd)
+        time.sleep(5)
