c7b03f21a4fdd596e2370694974c1dc04dd25580
Jim Dodgen <jim@dodgen.us>
2026-01-21 19:19:01 -0800

    Update helper_service_tool.py

--- linux/http_filter/src/helper_service_tool.py
+++ linux/http_filter/src/helper_service_tool.py
@@ -16,13 +16,13 @@ ExecStart=/bin/systemctl restart http_filter.service
 [Install]
 WantedBy=multi-user.target
 
-    ''' % (os.getcwd(), os.getcwd())
+'''
     print("=====\n%s\n=====\nInstall service? must be root (y,N)" % (service))
     ans = input()
     if (ans.upper() == "Y"):
         with open(systemd_path+service_name,"w") as text_file:
             text_file.write(service)
-        os.system(f"systemctl stop {service_name}")
+        '''os.system(f"systemctl stop {service_name}")
         os.system("systemctl daemon-reload")
 
         os.system(f"systemctl start {service_name}")
@@ -30,7 +30,7 @@ WantedBy=multi-user.target
 
         os.system(f"systemctl status {service_name}")
         os.system(f"journalctl -f -u {service_name}")
-    
+    '''
 
 if __name__ == "__main__":
     install()
