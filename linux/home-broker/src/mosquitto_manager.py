081f5f701b7ec566dd79b51b3a2df3e2e717a6c0
jdodgen <jim@dodgen.us>
2026-04-28 21:56:02 -0700

    removed task

--- linux/home-broker/src/mosquitto_manager.py
+++ linux/home-broker/src/mosquitto_manager.py
@@ -24,13 +24,13 @@ def print_always(*args, **kwargs): # replace print
 #
 #
 
-def is_mosquitto_alive():
-    result = pgrep("mosquitto")
-    if not result:
-        print("mosquitto not running")
-        return None
-    else:
-        return result[0]
+# def is_mosquitto_alive():
+    # result = pgrep("mosquitto")
+    # if not result:
+        # print("mosquitto not running")
+        # return None
+    # else:
+        # return result[0]
 
 def reload_config():
     with open(config.MOSQUITTO_FILE_PATH, "w") as mos_config:
@@ -43,22 +43,22 @@ def reload_config():
     #os.kill(pid, signal.SIGHUP)   # tells mosquitto to reload config
     return True
 
-def task():
-    # this allows us to control mosquitto
-    # If sleeping a reboot was expected to refresh mosquitto
-    # if a wake then we are now trying to start and stop mosquitto
-    result = reload_config()
-    if not result:
-        print("[mosquitto_manager] chmod reload_config detected mosquitto is not running")
-    while True:
-        # mosquitto happer when run as a service to this is just here to drop the config
-        # I need a signal or something to get a re-read from mosquitto. for now just need a reboot
-        #subprocess.run(["/usr/sbin/mosquitto", "-c", const.mosquitto_file_path,]) # '-v'])
-        ## never returns, I hope :)
-        #print("mosquitto exited, waiting and restarting")
-        # future will be 
-        time.sleep(config.MOSQUITTO_SLEEP_SECONDS)   # asleep as you see Zzzzzzzz
+# def not_any_more_task():
+    # # this allows us to control mosquitto
+    # # If sleeping a reboot was expected to refresh mosquitto
+    # # if a wake then we are now trying to start and stop mosquitto
+    # result = reload_config()
+    # if not result:
+        # print("[mosquitto_manager] chmod reload_config detected mosquitto is not running")
+    # while True:
+        # # mosquitto happer when run as a service to this is just here to drop the config
+        # # I need a signal or something to get a re-read from mosquitto. for now just need a reboot
+        # #subprocess.run(["/usr/sbin/mosquitto", "-c", const.mosquitto_file_path,]) # '-v'])
+        # ## never returns, I hope :)
+        # #print("mosquitto exited, waiting and restarting")
+        # # future will be 
+        # time.sleep(config.MOSQUITTO_SLEEP_SECONDS)   # asleep as you see Zzzzzzzz
 
 if __name__ == "__main__":
-    task()
-    print("Should not get here")
+    reload_config()
+    print("reloaded")
