0261b2bcab116f68ff7df6ffd5095809d044a8ac
Jim Dodgen <jim@dodgen.us>
2026-06-10 12:14:44 -0700

    added voice to nav

--- linux/alertaway/src/http_common.py
+++ linux/alertaway/src/http_common.py
@@ -118,8 +118,9 @@ def nav_section():
     nav =  {"nav_section": f'''
     <h1>AlertAway Toolbox</h1>
     <nav>
-        <a href="http://{ my_ip }:{ HTTP_MAIN_PORT }">MQTT/IP Devices + WeMo</a>
+        <a href="http://{ my_ip }:{ HTTP_MAIN_PORT }">MQTT/IP Devices</a>
         <a href="http://{ my_ip }:{ HTTP_MAIN_PORT }/zigbee2mqtt">zigbee2mqtt</a>
+        <a href="http://{ my_ip }:{ VOICE_PORT }">Voice</a>
         <a href="http://{ my_ip }:{ EVENTS_PORT }">Events</a>
         <a href="http://{ my_ip }:{ TRIGGERS_PORT }">Triggers</a>
         <a href="http://{ my_ip }:{ TIMERS_PORT }">Timers</a>
