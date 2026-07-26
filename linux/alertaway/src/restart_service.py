75081c7173883c82f0da1c97f0430ede28e38b9d
jdodgen <jim@dodgen.us>
2026-07-26 14:27:41 -0700

    changed prints to a returned message

--- linux/alertaway/src/restart_service.py
+++ linux/alertaway/src/restart_service.py
@@ -1,12 +1,17 @@
+
+#sudo visudo -f /etc/sudoers.d/python-restart
+#jim ALL=(ALL) NOPASSWD: /usr/bin/systemctl restart alertaway*.service
+
+
 import subprocess
 
 def restart(service_name):
     try:
         # Calls: sudo systemctl restart your_service.service
         subprocess.run(['sudo', 'systemctl', 'restart', service_name], check=True)
-        print(f"[restart_service] Service {service_name} restarted successfully.")
+        result = f"[restart_service] Service {service_name} restarted successfully."
     except subprocess.CalledProcessError as e:
-        print(f"[restart_service] Error restarting service: {e}")
-
+        result = f"[restart_service] Error restarting service: [{service_name}] {e}"
+    return result
 if __name__ == '__main__':
-    restart("alertaway-triggers-daemon")
+    print(f"result: {restart("alertaway-timers-daemon")}")
