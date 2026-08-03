4bf66e6306ab15f9b4b4ab3ad1a61ef94c31ee7f
Jim Dodgen <jim@dodgen.us>
2026-08-02 17:25:47 -0700

    added path to sudo

--- linux/alertaway/src/restart_service.py
+++ linux/alertaway/src/restart_service.py
@@ -7,7 +7,7 @@ import subprocess
 def restart(service_name):
     try:
         # Calls: sudo systemctl restart your_service.service
-        subprocess.run(['sudo', '/usr/bin/systemctl', 'restart', service_name], check=True)
+        subprocess.run(['/usr/bin/sudo', '/usr/bin/systemctl', 'restart', service_name], check=True)
         result = f"[restart_service] Service {service_name} restarted successfully."
     except subprocess.CalledProcessError as e:
         result = f"[restart_service] Error restarting service: [{service_name}] {e}"
