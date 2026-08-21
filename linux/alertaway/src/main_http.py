4288d60c9c2ca73183f059beba07d38e47a85145
jdodgen <jim@dodgen.us>
2026-08-21 12:10:28 -0700

    fixed up main_http

--- linux/alertaway/src/main_http.py
+++ linux/alertaway/src/main_http.py
@@ -4,7 +4,8 @@ import aiohttp_jinja2
 from aiohttp import web
 import database
 import re
-import message
+import paho.mqtt.publish as publish
+#import message
 import mqtt_hello
 import restart_service
 from fauxmo_manager import build_cfg
@@ -17,7 +18,7 @@ OUR_PORT =  config.HTTP_MAIN_PORT
 #MAIN_Q=None  #  messages queue
 
 DB=None # built per process
-msg=None   # built per process from message.message
+#msg=None   # built per process from message.message
 # conditional print
 import os 
 my_name = "http_server"
@@ -119,32 +120,30 @@ async def z2m_page(request):
     # Force direct dictionary rendering
     return aiohttp_jinja2.render_template('zigbee2mqtt.html', request, context=context)
 
-
-
 async def whoareyou(request):
     myhost = os.uname()[1]
     return web.Response(text=f"iam/{myhost}")
     
-async def create_voice(request):
-    error_msg = ''
-    # aiohttp requires awaiting the form data
-    if request.method == "POST":
-        data = await request.post()
-        action = data.get("action")
-        if action == "restart":
-            # do a systemd restart to pick up the fresh config
-             restart_service.restart("alertaway-fauxmo-task")
-             # pre systemd watch_dog_queue.put(["startfauxmotask", "start"])
-        elif action == "display":
-            cfg = build_cfg()  #"fauxmo_cfg_placeholder" # Replace with your manager call
-            return web.Response(text=f"<pre>{cfg}</pre>", content_type='text/html')
-        else:
-            print(f"Voice[{data}]")
-            if "voice_name" in data and "voice_device" in data:
-                DB.create_voice(data["voice_name"], data.get("port"), data["voice_device"])
-            else:
-                error_msg = 'Both Voice name and device required'
-    return await render_response(request, error_msg)
+# async def create_voice(request):
+    # error_msg = ''
+    # # aiohttp requires awaiting the form data
+    # if request.method == "POST":
+        # data = await request.post()
+        # action = data.get("action")
+        # if action == "restart":
+            # # do a systemd restart to pick up the fresh config
+             # restart_service.restart("alertaway-fauxmo-task")
+             # # pre systemd watch_dog_queue.put(["startfauxmotask", "start"])
+        # elif action == "display":
+            # cfg = build_cfg()  #"fauxmo_cfg_placeholder" # Replace with your manager call
+            # return web.Response(text=f"<pre>{cfg}</pre>", content_type='text/html')
+        # else:
+            # print(f"Voice[{data}]")
+            # if "voice_name" in data and "voice_device" in data:
+                # DB.create_voice(data["voice_name"], data.get("port"), data["voice_device"])
+            # else:
+                # error_msg = 'Both Voice name and device required'
+    # return await render_response(request, error_msg)
     
 def send_mqtt_publish(feature_rowid, true_or_false):
     print(f"send_mqtt_publish rowid[{feature_rowid}] [{true_or_false}]")
@@ -157,7 +156,8 @@ def send_mqtt_publish(feature_rowid, true_or_false):
         payload = true_value
     else:
         payload = false_value
-    message.publish_single(topic, payload, my_parent=my_name)
+    publish.single(topic, payload)
+    return (topic,payload)
     
 async def all_devices(request):
     error = ""
@@ -172,17 +172,17 @@ async def all_devices(request):
         ## parts = request.form["action"].split("/")
         print("/all_devices action part 0 [%s]" % parts[0])
         if parts[0] == "send":
-            send_mqtt_publish(parts[2], parts[1])
+            (topic, payload) = send_mqtt_publish(parts[2], parts[1])
+            error=f"Command {topic} > {payload} published"
         elif parts[0] == "zbrefresh":
-            # subscribe.simple(const.zigbee2mqtt_bridge_devices, hostname=message.our_ip_address())
-            msg.subscribe(config.ZIGBEE2MQTT_BRIDGE_DEVICES)
+            publish.single(config.ZIGBEE2MQTT_BRIDGE_REQUEST_DEVICES,"")
             error="ZigBee devices refreshing"
         elif parts[0] == "delete_old":
             # subscribe.simple(const.zigbee2mqtt_bridge_devices, hostname=message.our_ip_address())
             db.delete_marked_devices()
             error="marked zero-date devices removed"
         elif parts[0] == "iprefresh":
-            msg.publish(mqtt_hello.hello_request_topic, my_name) 
+            publish.single(mqtt_hello.hello_request_topic) 
             error="Auto IP devices refreshed"
         elif parts[0] == 'delete':
             DB.delete_feature(parts[1],parts[2])
@@ -220,8 +220,8 @@ app.add_routes([
     web.get('/whoareyou', whoareyou),
     web.get('/create_IP_device', create_IP_device),
     web.post('/create_IP_device', create_IP_device),
-    web.get('/create_voice', create_voice),
-    web.post('/create_voice', create_voice),
+    # web.get('/create_voice', create_voice),
+    # web.post('/create_voice', create_voice),
     #web.get('/remove_voice', remove_voice),
     #web.post('/remove_voice', remove_voice),
     web.get('/all_devices', all_devices),
@@ -230,28 +230,28 @@ app.add_routes([
     web.post('/zigbee2mqtt', z2m_page),
 ])
 
-def task(fauxmo):
-    global fauxmo_task
-    global DB
-    global msg
-#    global MAIN_Q
+# def task(fauxmo):
+    # global fauxmo_task
+    # global DB
+    # global msg
+# #    global MAIN_Q
 
-    DB=database.database()
+    # DB=database.database()
     
-    DB_NAME =   config.DB_NAME
- #   MAIN_Q = queue.Queue() 
+    # DB_NAME =   config.DB_NAME
+ # #   MAIN_Q = queue.Queue() 
     
-#    msg = message.message(MAIN_Q, my_parent=my_name)
-    msg.subscribe(config.ZIGBEE2MQTT_BRIDGE_DEVICES)
-    msg.publish(mqtt_hello.hello_request_topic, my_name)
-    fauxmo_task = fauxmo
-    # os.nice(-1)
-    print("Starting http server...")
-    try:
-        web.run_app(app, port=OUR_PORT)
-        print("http_server.serve_forever we should not get here")
-    except Exception as e:
-        print("Error during web.run_app:", e)
+    # msg = message.message(MAIN_Q, my_parent=my_name)
+    # msg.subscribe(config.ZIGBEE2MQTT_BRIDGE_DEVICES)
+    # msg.publish(mqtt_hello.hello_request_topic, my_name)
+    # fauxmo_task = fauxmo
+    # # os.nice(-1)
+    # print("Starting http server...")
+    # try:
+        # web.run_app(app, port=OUR_PORT)
+        # print("http_server.serve_forever we should not get here")
+    # except Exception as e:
+        # print("Error during web.run_app:", e)
     
 if __name__ == '__main__':
     DB=database.database()
